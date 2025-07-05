5f15cbf2e0fa38237c5ebf0b370cbb0226db630e
ThisaraAriyawansha <thisara.a2001@gmail.com>
2025-07-05 12:59:32 +0530

    update billing

--- resources/views/sales/billing.blade.php
+++ resources/views/sales/billing.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>{{ $settings[6]->value}} | Items</title>
+    <title>{{ $settings[6]->value}} | Billing</title>
     <link rel="icon" href="../{{ $settings[13]->value}}">
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
