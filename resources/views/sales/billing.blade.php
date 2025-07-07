74a3617074d86673f2775ca85ab0a8549b53fcbb
ThisaraAriyawansha <thisara.a2001@gmail.com>
2025-07-07 14:05:23 +0530

    update

--- resources/views/sales/billing.blade.php
+++ resources/views/sales/billing.blade.php
@@ -307,7 +307,16 @@
                         </select>
                     </div>
 
-
+                    <!-- Add Customer Button -->
+                    <button type="button" data-modal-target="customer-modal" data-modal-toggle="customer-modal"
+                            class="hidden flex items-center justify-center px-4 py-2.5 text-sm font-medium text-white rounded-lg transition-all hover:opacity-90"
+                            style="background-color: {{ $settings[7]->value }}">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 16 16">
+                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+                        </svg>
+                        Add Customer
+                    </button>
 
                 </div>
 
